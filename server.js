968bdf8ffa2ba9c7e338b59ac64084b65205c7a1
TheIronG <theirongman@gmail.com>
2018-05-03 20:08:58 -0400

    Changed folder name to be more clear
    Changed the "java" folder name to "java-edition", to better describe what the contents are

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const NodecraftServer = require('./src/java/server');
+const NodecraftServer = require('./src/java-edition/server');
 new NodecraftServer('1.12.2', __dirname, {
 	motd: '§bWelcome to §eNodecraft',
 	max_players: 100,
